f5c1a8f5cdc750de791ded0a7f03983632f346cf
范声乐 <fanshengyue@fanshengyuedemac-mini.local>
2018-09-27 15:53:29 +0800

    Initial Commit

---
+++ NewMinister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewMinister
+//
+//  Created by 范声乐 on 2018/9/27.
+//  Copyright © 2018年 范声乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewMinister/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NewMinister
+//
+//  Created by 范声乐 on 2018/9/27.
+//  Copyright © 2018年 范声乐. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
