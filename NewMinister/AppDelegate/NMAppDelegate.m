103206a66a34f4cd031a0eeebe6b40dbeb63d68f
范声乐 <fanshengyue@fanshengyuedeMac-mini.local>
2018-10-11 19:08:32 +0800

    防守打法两节课
    Signed-off-by: 范声乐 <fanshengyue@fanshengyuedeMac-mini.local>

--- NewMinister/AppDelegate/NMAppDelegate.m
+++ NewMinister/AppDelegate/NMAppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //Gigi的  
     //设置跟视图控制器
     [self setUpRootViewController];
     
